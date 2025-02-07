0f635a526a835962481be5ffc8124611eece77d6
Tymee Kong <tymeekong562@gmail.com>
2025-02-07 15:27:35 -0800

    User can now logout

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+'use client';
+import { Navbar, NavbarBrand, NavbarContent, Link } from '@heroui/react';
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react';
 
 const Nav = () => {
-  return <div></div>;
+  const router = useRouter();
+  const handleLogout = async () => {
+    const response = await fetch(
+      'https://frontend-take-home-service.fetch.com/auth/logout'
+    );
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    router.push('/login');
+  };
+
+  return (
+    <Navbar>
+      <NavbarBrand>
+        <p>Doggos</p>
+      </NavbarBrand>
+      <NavbarContent>
+        <Link href='/login' onPress={handleLogout}>
+          Logout
+        </Link>
+      </NavbarContent>
+    </Navbar>
+  );
 };
 
 export default Nav;
