185ee751fa17ecc1f507af8bc3c96695968820dd
Tymee Kong <tymeekong562@gmail.com>
2025-02-08 21:14:44 -0800

    Added lost code in page.tsx

--- app/dogs/page.tsx
+++ app/dogs/page.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import DogList from './doglist';
+import DogFilter from './dogfilter';
+import { ScrollShadow } from '@heroui/react';
+import { useState } from 'react';
+
+export interface Dog {
+  id: string;
+  img: string;
+  name: string;
+  age: number;
+  zip_code: string;
+  breed: string;
+}
 
 const Dogs = () => {
+  const [dogs, setDogs] = useState<Dog[]>([]);
   return (
     <div>
-      <h1>Dogs</h1>
+      <ScrollShadow>
+        <DogFilter setDogs={setDogs} />
+      </ScrollShadow>
       <DogList />
     </div>
   );
