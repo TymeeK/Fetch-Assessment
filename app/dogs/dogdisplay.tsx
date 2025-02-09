4f1655f04e07eacdc32ecc56ad48a031ec4e9b05
Tymee Kong <tymeekong562@gmail.com>
2025-02-08 22:43:55 -0800

    Added pagination

--- app/dogs/dogdisplay.tsx
+++ app/dogs/dogdisplay.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Button, Card } from '@heroui/react';
+import { Button, Card, Pagination } from '@heroui/react';
 import { useEffect, useState } from 'react';
 import { Dog } from './page';
 interface DogDisplayProps {
@@ -7,23 +7,28 @@ interface DogDisplayProps {
 }
 
 const DogDisplay: React.FC<DogDisplayProps> = ({ dogs }) => {
+  const dogsPerPage = 6;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const indexOfLastDog = currentPage * dogsPerPage;
+  const indexOfFirstDog = indexOfLastDog - dogsPerPage;
+  const currentDogs = dogs.slice(indexOfFirstDog, indexOfLastDog);
+
   return (
     <div className='grid sm:grid-cols-1 md:grid-cols-3 '>
-      {dogs.map((dog: Dog) => (
+      {currentDogs.map((dog: Dog) => (
         <Card className=' max-w-sm m-10 ' key={dog.id}>
           <div className='flex m-2 justify-center'>
             <h1 className='font-bold text-center text-2xl'>{dog.name}</h1>
           </div>
           <img
-            className='w-full h-48 object-cover rounded'
+            className='w-full h-64 object-cover rounded'
             src={dog.img}
             alt={dog.name}
           />
           <div className='flex flex-row p-3'>
             <div className='flex justify-center items-center w-1/2'>
               <label>{dog.breed}</label>
-              {/* <label>{dog.age} years old</label>
-              <label>{dog.zip_code}</label> */}
             </div>
             <div className='w-1/2 flex justify-end items-center '>
               <Button color='primary' variant='ghost'>
@@ -33,6 +38,13 @@ const DogDisplay: React.FC<DogDisplayProps> = ({ dogs }) => {
           </div>
         </Card>
       ))}
+      <div className='flex justify-center w-screen mb-5 '>
+        <Pagination
+          total={Math.ceil(dogs.length / dogsPerPage)}
+          initialPage={1}
+          onChange={page => setCurrentPage(page)}
+        />
+      </div>
     </div>
   );
 };
