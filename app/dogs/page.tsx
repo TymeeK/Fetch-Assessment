24a6d18218f01da7daf47d01f26b219a48a73b5a
Tymee Kong <tymeekong562@gmail.com>
2025-02-12 16:08:19 -0800

    Created a modal and abstracted a bunch of functions into smaller components

--- app/dogs/page.tsx
+++ app/dogs/page.tsx
@@ -10,6 +10,12 @@ import {
   DropdownMenu,
   DropdownItem,
   CircularProgress,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  useDisclosure,
+  ModalFooter,
 } from '@heroui/react';
 import { useState } from 'react';
 import Nav from '@/components/navbar';
@@ -23,15 +29,17 @@ export interface Dog {
   breed: string;
 }
 
-export const sortArrayByBreed = (dogs: Dog[]) => {
+export const sortArrayByBreed = (dogs: Dog[]): Dog[] => {
   return dogs.sort((a, b) => a.breed.localeCompare(b.breed));
 };
 
-const Dogs = () => {
+const Dogs: React.FC = () => {
   const [dogs, setDogs] = useState<Dog[]>([]);
-  const [favoriteDogs, setFavoriteDogs] = useState<Dog[]>([]);
+  const [favoriteDogId, setFavoriteDogId] = useState<string[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [initialFetch, setIntialFetch] = useState(false);
+  const [dogMatch, setDogMatch] = useState<Dog | null>(null);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   const handleDogFilter = (key: string) => {
     let sortedDogs: Dog[] = [...dogs];
@@ -59,9 +67,114 @@ const Dogs = () => {
     }
     setDogs(sortedDogs);
   };
+
+  const matchFavoriteDog = async () => {
+    try {
+      const response = await fetch(
+        'https://frontend-take-home-service.fetch.com/dogs/match',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+          body: JSON.stringify(favoriteDogId),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const { match } = await response.json();
+
+      const matchResponse = await fetch(
+        'https://frontend-take-home-service.fetch.com/dogs/',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+          body: JSON.stringify([match]),
+        }
+      );
+      const matchedDog = await matchResponse.json();
+      console.log(matchedDog);
+      setDogMatch(matchedDog[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const LoadingBar: React.FC = () => {
+    return (
+      <div className='flex justify-center items-center h-screen w-screen'>
+        <CircularProgress
+          color='default'
+          size='lg'
+          label='Fetching doggos...'
+        />
+      </div>
+    );
+  };
+
+  const InitialText: React.FC = () => {
+    return (
+      <div className='flex justify-center w-screen '>
+        <h3 className='text-l font-bold text-gray-400 pt-20'>
+          No dogs found. Press the filter button to search for dogs!
+        </h3>
+      </div>
+    );
+  };
+
+  // const adoptDog = () => {
+
+  // };
+
+  const DogMatchModal: React.FC = () => {
+    return (
+      <Modal placement='center' isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {onClose => (
+            <>
+              <ModalHeader className='text-center font-bold text-2xl'>
+                Match!
+              </ModalHeader>
+              <ModalBody>
+                {dogMatch && (
+                  <div className='flex flex-col'>
+                    <h1 className='font-bold text-2xl'>{dogMatch.name}</h1>
+                    <img src={dogMatch.img} alt={dogMatch.name} />
+                    <label>Breed: {dogMatch.breed}</label>
+                    <label>Zip code:{dogMatch.zip_code}</label>
+                    <label>Age: {dogMatch.age}</label>
+                  </div>
+                )}
+              </ModalBody>
+              <ModalFooter>
+                <Button color='danger' variant='light' onPress={onClose}>
+                  Close
+                </Button>
+                <Button
+                  color='success'
+                  variant='bordered'
+                  onPress={() => adoptDog()}
+                >
+                  Adopt
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    );
+  };
+
   return (
     <>
       <Nav />
+      <DogMatchModal />
       <ScrollShadow>
         <DogFilter
           setDogs={setDogs}
@@ -87,20 +200,20 @@ const Dogs = () => {
         </div>
       )}
 
-      {loading && (
-        <div className='flex justify-center items-center h-screen w-screen'>
-          <CircularProgress
-            color='default'
-            size='lg'
-            label='Fetching doggos...'
-          />
-        </div>
-      )}
-      {!loading && dogs.length === 0 && !initialFetch && (
-        <div className='flex justify-center w-screen '>
-          <h3 className='text-l font-bold text-gray-400 pt-20'>
-            No dogs found. Press the filter button to search for dogs!
-          </h3>
+      {loading && initialFetch && dogs.length === 0 && <LoadingBar />}
+      {!loading && dogs.length === 0 && !initialFetch && <InitialText />}
+
+      {favoriteDogId && favoriteDogId.length > 0 && (
+        <div className='flex justify-center w-screen'>
+          <label>You have selected {favoriteDogId.length} favorite dogs</label>
+          <Button
+            onPress={() => {
+              matchFavoriteDog();
+              onOpen();
+            }}
+          >
+            Match!
+          </Button>
         </div>
       )}
 
@@ -108,9 +221,8 @@ const Dogs = () => {
         <div className='flex justify-center w-screen'>
           <DogDisplay
             dogs={dogs}
-            favoriteDogs={favoriteDogs}
-            loading={loading}
-            setLoading={setLoading}
+            favoriteDogId={favoriteDogId}
+            setFavoriteDogId={setFavoriteDogId}
           />
         </div>
       )}
